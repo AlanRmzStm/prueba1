e263aea6ca700f1ca384eda8ffce31c520da9f81
Alan Ramirez <alan.rmz.stm@gmail.com>
2021-03-04 22:35:28 -0600

    intento

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            premium: true,
+            cart: [],
+            premium: true
         }
     },
-    methods: {     
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        }
     }
-    
-})
+  })
+  
