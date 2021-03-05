8eb7c035ac7a6b42afe06f88684fa698968f8976
Alan Ramirez <alan.rmz.stm@gmail.com>
2021-03-04 20:08:36 -0600

    Lesson 9 incomplete

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0
+            cart: 0,
+            premium: true,
+            details: "Es un producto muy elegante"
         }
     },
-    methods: {
-      
-    },
+    methods: {     
+    }
     
 })
